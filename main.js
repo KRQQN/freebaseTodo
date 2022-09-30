5ec52297a42f48bcdb5203b7ceb828684ad01b0c
Rkron91 <robin.kron@lerniastudent.se>
2022-09-30 12:08:38 +0200

    counterfixing

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const inputTxt = document.querySelector("#txtFieldInput");
 const addTaskBtn = document.querySelector("#addTaskBtn");
 const list = document.querySelector(".todo-list");
@@ -10,61 +9,59 @@ let countValue = 0;
 // adding eventlistener -> addTaskBtn.
 addTaskBtn.addEventListener("click", function (e) {
   e.preventDefault();
-  //getting text from <input> through inputTxt -> text 
+  //getting text from <input> through inputTxt -> text
   const text = inputTxt.value;
   //if inputfield isnt empty
   if (text != "") {
     //defaults errorMsg & inputTxt each click
     errorMsg.innerHTML = "";
     inputTxt.value = "";
-    //declares and sets variables to html-element
+    //declares and sets variables to link to html-element
     const task = document.createElement("li");
     const taskLabel = document.createElement("span");
-    const trashbutton = document.createElement("button");
-    trashbutton.innerHTML = "&#128465";
-    trashbutton.setAttribute("id","deleteBtn");
+    const trashButton = document.createElement("button");
     
+    trashButton.innerHTML = "&#128465";
+    trashButton.setAttribute("id", "deleteBtn");
+    //appendsession
     list.appendChild(task);
     taskLabel.innerText = text;
     task.appendChild(taskLabel);
-    task.appendChild(trashbutton);
+    task.appendChild(trashButton);
 
+    //object made and pushed into array
     const taskObj = { task: text, isComplete: false };
     taskArray.push(taskObj);
 
-    trashbutton.addEventListener("click",
-    function(){
-        if(task.getAttribute("class") == "completed"){
-            countValue--;
-            task.remove();
-        } else {
-            task.remove();
-        }
-        count.innerHTML = `Tasks done: ${countValue}`;
-        
+    // Eventlistener for deleteBtn"trashButton
+    trashButton.addEventListener("click", function () {
+      if (task.getAttribute("class") == "completed") {
+        countValue--;
+        task.remove();
+      } else {
+        task.remove();
+      }
+      if (countValue == 0) count.innerHTML ="";
+      if (countValue > 0) count.innerHTML = `Tasks done: ${countValue}`;
+    });
 
-
-    })
-
+    //Eventlistener for tasklabel.
     taskLabel.addEventListener("click", function () {
       if (task.getAttribute("class") == "completed") {
         task.setAttribute("class", "");
         taskObj.isComplete = false;
         countValue--;
-        
-        
       } else {
         task.setAttribute("class", "completed");
         taskObj.isComplete = true;
         countValue++;
-        
       }
-      count.innerHTML = `Tasks done: ${countValue}`;
+      if (countValue == 0) count.innerHTML ="";
+      if (countValue > 0) count.innerHTML = `Tasks done: ${countValue}`;
+
+      
     });
   } else {
     errorMsg.innerHTML = "Du måste skriva in något i textfältet";
   }
-  
 });
-
-
